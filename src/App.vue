ef7a4ebff03efbda7734b3635d8cc59d93fcd809
moxccc <1355926601@qq.com>
2024-04-24 14:04:29 +0800

    1.初始化安装了 vue3 vite vue-router pinia lass scss

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <routerView></routerView>
+</template>
+
+<style scoped></style>
---
+++ src/api/index.js
@@ -0,0 +1,28 @@
+// import instance from ;
+import instance from '../utils/request'
+
+// 登录
+// export const Login=(data)=>instance.post("/api/manager/login",data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
