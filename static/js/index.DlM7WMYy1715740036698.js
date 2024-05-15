f631099b5c5d89d240eea977d4ee96269e42ebba
moxccc <100174357+moxccc@users.noreply.github.com>
2024-05-15 02:27:18 +0000

    Deploying to git-pages from @ moxccc/tools@a53e7a379f9e39c2bf0e9b1df49b51474aa21bbd 🚀

---
+++ static/js/index.DlM7WMYy1715740036698.js
@@ -0,0 +1,1 @@
+import{c as t,t as a,u as n,o as r}from"./index.8RvH33Es1715740036698.js";const _={__name:"index",setup(o){let e="正式";return(s,c)=>(r(),t("div",null," 这个是"+a(n(e))+"版本 ",1))}};export{_ as default};
